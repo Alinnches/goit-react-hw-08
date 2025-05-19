acac9adc8167c511aef65030a2db02ea3efa197a
Alina Strijkiv <alinastrijkiv@gmail.com>
2025-05-19 20:17:47 +0300

    fixed

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contacts/operations";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import s from "./ContactsPage.module.css";
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={s.container}>
       <h2>Contacts</h2>
